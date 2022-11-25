bca5b2e252f1e84c0c6cf1b6c66695ef766adb83
Hilora Dewett <h.dewett@outlook.com>
2022-11-25 17:18:27 -0500

    changed request func to print message when breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,9 +5,11 @@ const breedName = args[0];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
   const data = JSON.parse(body);
-  console.log(data);
-});
 
+  if (data.length === 0){
+    console.log("Breed name not found.");
 
-// const parseData = JSON.parse(data);
-// console.log(parseData["description"]);
+  } else {
+    console.log(data[0]["description"]);
+  }
+  });
