8750eb20fb2066036dcc6af5f00c8b213a870191
Hilora Dewett <h.dewett@outlook.com>
2022-11-25 17:02:52 -0500

    changed description text to object in request

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,8 +3,9 @@ const args = process.argv.slice(2);
 const breedName = args[0];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q= ${breedName}`, function(error, response, body) {
-  console.log(body);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+  const data = JSON.parse(body);
+  console.log(data);
 });
 
 
